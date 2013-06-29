707ee7e5fa42c95a715634aac9e6201a7495bc94
Thys meintjes <tmeintjes@ccg.murdoch.edu.au>
2013-06-29 22:08:00 +0800

    Added more models from sheets

---
+++ melanoma/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from melanoma.models import *
+
+admin.site.register(Sample)
+admin.site.register(Facility)
+admin.site.register(Contact)
+admin.site.register(Affiliation)
+admin.site.register(DNASource)
--- melanoma/models.py
+++ melanoma/models.py
@@ -1,11 +1,24 @@
 from django.db import models
 
 
+class Affiliation(models.Model):
+    """
+    Affiliation
+    """
+    name = models.CharField(max_length=100)
+
+
+class DNASource(models.Model):
+    """
+    DNA Source
+    """
+    source = models.CharField(max_length=100)
+
+
 class Facility(models.Model):
     """
     The Sequencing Facility
     """
-
     name = models.CharField(max_length=100)
 
 
@@ -13,8 +26,10 @@ class Contact(models.Model):
     """
     Contact Detail
     """
-    
     name = models.CharField(max_length=200)
+    affiliation = models.ForeignKey(Affiliation)
+    email = models.CharField(max_length=200)
+
 
 class Sample(models.Model):
     """
@@ -26,7 +41,7 @@ class Sample(models.Model):
     requested_sequence_coverage = models.CharField(max_length=4)
     species = models.CharField(max_length=100)
     sequencing_faciltiy = models.ForeignKey(Facility)
+    dna_source = models.ForeignKey(DNASource)
 
-
-
+    sex = models.CharField(max_length=1)
 
