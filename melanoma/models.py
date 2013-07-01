088da8e47f075f686deee5101183c39f964cb8fb
Thys Meintjes <tmeintjes@ccg.murdoch.edu.au>
2013-07-01 10:10:42 +0800

    Full first model for melanoma

--- melanoma/models.py
+++ melanoma/models.py
@@ -9,7 +9,7 @@ class Affiliation(models.Model):
     description = models.CharField(max_length=200)
 
     def __unicode__(self):
-	return self.name
+        return self.name
 
 
 class DNASource(models.Model):
@@ -20,19 +20,39 @@ class DNASource(models.Model):
 
 
     def __unicode__(self):
-	return self.source
+        return self.source
 
 
 class Facility(models.Model):
     """
-    The Sequencing Facility
+    The Sequencing SequencingFacility
     """
     name = models.CharField(max_length=100)
-
+    service = models.CharField(max_length=100)
 
     def __unicode__(self):
-	return self.name
+        return self.name
 
+
+class Sequencer(models.Model):
+    """
+    The Sequencer
+    """
+    name = models.CharField(max_length=100)
+        
+    def __unicode__(self):
+        return self.name
+
+
+class TumorStage(models.Model):
+    """
+    Tumor Stage
+    """
+    
+    name = models.CharField(max_length=100)
+
+    def __unicode__(self):
+        return self.name
 
 
 class Contact(models.Model):
@@ -45,7 +65,7 @@ class Contact(models.Model):
 
 
     def __unicode__(self):
-	return self.name
+        return self.name
 
 
 
@@ -59,14 +79,48 @@ class Sample(models.Model):
     sample_name = models.CharField(max_length=200)
     requested_sequence_coverage = models.CharField(max_length=4)
     species = models.CharField(max_length=100)
+    
     sequencing_faciltiy = models.ForeignKey(Facility)
+    array_analysis_faciltiy = models.ForeignKey(Facility)
+    whole_genome_sequencing_faciltiy = models.ForeignKey(Facility)    
+    
+    date_sent_to_sequencing_facility = models.DateField()
+    date_recieved_from_sequencing_facility = models.DateField()
+    
     dna_source = models.ForeignKey(DNASource)
+    
+    # Library
+    # FIXME, factor out ? 
+    library = models.CharField(max_length=20)
+    library_construction = models.CharField(max_length=20)
+    library_construction_protocol = models.CharField(max_length=100)
+    
+    index_number = models.IntegerField()
+    sequencer = models.ForeignKey(Sequencer)
+    run_number = models.IntegerField()
+    flow_cell_id = models.CharField(max_length=10)
+    lane_number = models.IntegerField()
 
     sex = models.CharField(max_length=1)
+    tumor_stage = models.ForeignKey(TumorStage)
+    histological_subtype = models.CharField(max_length=50)
+    passage_number = models.IntegerField()
+    dna_extraction_protocol = models.CharField(max_length=200)
+    
+    contact = models.ForeignKey(Contact)
+    
+    sequence_facility_filename = models.CharField(max_length=300)
+    md5cheksum =  models.CharField(max_length=32)
+    bpa_archive_url = models.URLField()    
+    analysed = models.BooleanField()
+    analysed_url = models.URLField()    
+    ftp_url = models.URLField()
+    
+    
 
 
     def __unicode__(self):
-	return self.bpa_sample_id + " " + self.sample_name
+        return self.bpa_sample_id + " " + self.sample_name
 
 
 
