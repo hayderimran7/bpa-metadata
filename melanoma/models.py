62b039a52328b265186f3098ab057e3300921dc3
Thys Meintjes <tmeintjes@ccg.murdoch.edu.au>
2013-07-01 14:22:47 +0800

    Added Array Model. Added BPA_ID Model. Added unique contraints on some fields.

--- melanoma/models.py
+++ melanoma/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+GENDERS = (('M', 'Male'), ('F', 'Female'), ('U', 'Unknown'),)
 
 class Affiliation(models.Model):
     """
@@ -61,13 +62,57 @@ class Contact(models.Model):
     """
     name = models.CharField(max_length=200)
     affiliation = models.ForeignKey(Affiliation)
-    email = models.CharField(max_length=200)
+    email = models.EmailField()
 
 
     def __unicode__(self):
         return self.name
 
+class LibraryProtocol(models.Model):
+    """
+    Library Protocol 
+    """
+    description = models.CharField(max_length=200)
+    
+    def __unicode__(self):
+        return self.description
+    
 
+class Library(models.Model):
+    """
+    Library
+    """
+    
+    paired_end = models.BooleanField()
+    base_pairs = models.IntegerField()
+    protocol = models.ForeignKey(LibraryProtocol)
+    
+    def __unicode__(self):
+        return "Size: " + str(self.base_pairs) + " Paired: " + str(self.paired_end) + " Protocol: " + str(self.protocol)
+
+
+class BPA_ID(models.Model):
+    """
+    BPA Generated ID 
+    """
+    bpa_id = models.CharField(max_length=16, unique=True)
+
+    def __unicode__(self):
+        return self.bpa_id
+
+class Array(models.Model):
+    """
+    Array
+    """
+    
+    bpa_id = models.ForeignKey(BPA_ID)
+    array_id = models.CharField(max_length=17, unique=True)
+    batch_number = models.IntegerField()
+    well_id = models.CharField(max_length=4)
+    MIA_id = models.CharField(max_length=50)
+    call_rate = models.FloatField()
+    
+    gender = models.CharField(max_length=1, choices=GENDERS)
 
 
 class Sample(models.Model):
@@ -75,7 +120,7 @@ class Sample(models.Model):
     A sample
     """
 
-    BPA_sample_id = models.CharField(max_length=16)
+    bpa_id = models.ForeignKey(BPA_ID)
     sample_name = models.CharField(max_length=200)
     requested_sequence_coverage = models.CharField(max_length=4)
     species = models.CharField(max_length=100)
@@ -89,11 +134,7 @@ class Sample(models.Model):
     
     dna_source = models.ForeignKey(DNASource)
     
-    # Library
-    # FIXME, factor out ? 
-    library = models.CharField(max_length=20)
-    library_construction = models.CharField(max_length=20)
-    library_construction_protocol = models.CharField(max_length=100)
+    library = models.ForeignKey(Library)
     
     index_number = models.IntegerField()
     sequencer = models.ForeignKey(Sequencer)
@@ -101,7 +142,8 @@ class Sample(models.Model):
     flow_cell_id = models.CharField(max_length=10)
     lane_number = models.IntegerField()
 
-    sex = models.CharField(max_length=1)
+    sex = models.CharField(choices=GENDERS, max_length=1)
+    
     tumor_stage = models.ForeignKey(TumorStage)
     histological_subtype = models.CharField(max_length=50)
     passage_number = models.IntegerField()
@@ -110,7 +152,7 @@ class Sample(models.Model):
     contact = models.ForeignKey(Contact)
     
     sequence_facility_filename = models.CharField(max_length=300)
-    md5cheksum =  models.CharField(max_length=32)
+    md5cheksum = models.CharField(max_length=32)
     BPA_archive_url = models.URLField()    
     analysed = models.BooleanField()
     analysed_url = models.URLField()    
@@ -121,6 +163,7 @@ class Sample(models.Model):
 
     def __unicode__(self):
         return self.bpa_sample_id + " " + self.sample_name
+
 
 
 
