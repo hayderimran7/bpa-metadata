534048f4563be91ed06e9581e6dd1101f24a049e
Thys Meintjes <tmeintjes@ccg.murdoch.edu.au>
2013-07-03 16:00:14 +0800

    Flattened out model

--- metadata/admin.py
+++ metadata/admin.py
@@ -1,15 +1,67 @@
 from django.contrib import admin
-from common.models import *
+from metadata.models import *
+
+class SampleAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Sample Identification', {'fields': [('bpa_id', 'sample_name')]}),
+        ('Sample Type', {'fields' : [('sex', 'tumor_stage', 'histological_subtype')]}),
+        (None, {'fields': ['date_sent_to_sequencing_facility', 'contact', 'note']}),
+    ]
+    
+    
+class LibraryAdmin(admin.ModelAdmin):
+    list_display = ('base_pairs', 'type', 'protocol')
+
+    
+    
+class RunAdmin(admin.ModelAdmin):
+     fieldsets = [       
+        ('Facilities', {'fields': [('sequencing_faciltiy', 'array_analysis_faciltiy', 'whole_genome_sequencing_faciltiy')
+                                   ]
+                        }
+        ),
+        ('Sequencing', {'fields': [('library', 'index_number'),
+                                   ('sequencer', 'run_number', 'flow_cell_id', 'lane_number'),
+                                   'DNA_extraction_protocol',
+                                   'passage_number'                        
+                                   ]
+                        }
+         ),
+    ]
+    
+    
+    
+class SequenceFileAdmin(admin.ModelAdmin):
+     fieldsets = [       
+      
+        (None, {'fields' : [('filename', 'md5cheksum'),
+                              'BPA_archive_url',
+                              ('analysed', 'analysed_url'),
+                              'ftp_url',                              
+                              'date_received_from_sequencing_facility'
+                              ]
+                  }
+         ),
+    ]
+    
+class AffiliationAdmin(admin.ModelAdmin):
+    fields = (('name', 'description'), )
+    list_display = ('name', 'description')
+    
 
 admin.site.register(Contact)
 admin.site.register(Facility)
 admin.site.register(BPASampleID)
 admin.site.register(Organism)
-
-class AffiliationAdmin(admin.ModelAdmin):
-    fields = (('name', 'description'), )
-    list_display = ('name', 'description')
-
+admin.site.register(Sample, SampleAdmin)
+admin.site.register(Library, LibraryAdmin)
+admin.site.register(Run, RunAdmin)
+admin.site.register(SequenceFile, SequenceFileAdmin)
+admin.site.register(DNASource)
+admin.site.register(LibraryProtocol)
+admin.site.register(Array)
+admin.site.register(TumorStage)
+admin.site.register(Sequencer)
 admin.site.register(Affiliation, AffiliationAdmin)
     
     
