f85be152ec0a18fc4dacdc888fb1e4e8e3a9909d
Thys meintjes <tmeintjes@ccg.murdoch.edu.au>
2013-07-12 21:40:09 +0800

    Unipath does path resolution

--- bpam/bpametadata/settings/base.py
+++ bpam/bpametadata/settings/base.py
@@ -2,6 +2,7 @@
 
 import os
 import socket
+from unipath import Path
 from django.core.exceptions import ImproperlyConfigured
 
 def get_env_variable(var_name):
@@ -12,8 +13,20 @@ def get_env_variable(var_name):
         error_msg = "Set the %s environment variable" % var_name
         raise ImproperlyConfigured(error_msg)
 
+PROJECT_DIR = Path(__file__).ancestor(3)
 
-PROJECT_PATH = os.path.realpath(os.path.dirname(__file__))
+MEDIA_ROOT = PROJECT_DIR.child("media")
+
+STATIC_ROOT = PROJECT_DIR.child("static")
+
+STATICFILES_DIRS = (
+    PROJECT_DIR.child("assets"),
+)
+
+TEMPLATE_DIRS = (
+    PROJECT_DIR.child("templates"),
+)
+
 
 DEBUG=False
 
@@ -63,31 +76,14 @@ USE_L10N = True
 # If you set this to False, Django will not use timezone-aware datetimes.
 USE_TZ = True
 
-# Absolute filesystem path to the directory that will hold user-uploaded files.
-# Example: "/var/www/example.com/media/"
-MEDIA_ROOT = ''
-
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://example.com/media/", "http://media.example.com/"
 MEDIA_URL = ''
 
-# Absolute path to the directory static files should be collected to.
-# Don't put anything in this directory yourself; store your static files
-# in apps' "static/" subdirectories and in STATICFILES_DIRS.
-# Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
-
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
 STATIC_URL = '/static/'
-
-# Additional locations of static files
-STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-)
 
 # List of finder classes that know how to find static files in
 # various locations.
@@ -121,8 +117,6 @@ ROOT_URLCONF = 'bpametadata.urls'
 
 # Python dotted path to the WSGI application used by Django's runserver.
 WSGI_APPLICATION = 'bpametadata.wsgi.application'
-
-TEMPLATE_DIRS = (PROJECT_PATH + '/templates/')
 
 INSTALLED_APPS = (
     'django.contrib.auth',
