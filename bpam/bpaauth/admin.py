6595f66ed74a1e0164dcd5b4a8897723ac6d8975
Thys Meintjes <tmeintjes@ccg.murdoch.edu.au>
2013-07-31 09:12:55 +0800

    Added extra personal fields

--- bpam/bpaauth/admin.py
+++ bpam/bpaauth/admin.py
@@ -23,8 +23,16 @@ class BPAUserAdmin(UserAdmin):
     
     list_display = ('username', 'title', 'department', 'first_name', 'last_name', 'email')
     list_filter = ('groups', 'last_name')
-    fieldsets = UserAdmin.fieldsets + (('Note', {'fields': ('title',)}),)
+    fieldsets = UserAdmin.fieldsets + (('Note', {'fields': ('note',)}),)
+    print(fieldsets)
 
+    fieldsets = (
+                 (None, {'fields': ('username', 'password')}),
+                 ('Personal Info', {'fields': ('first_name', 'last_name', 'title', 'department', 'email', 'telephone')}),
+                 ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+                 ('Important Dates', {'fields': ('last_login', 'date_joined')}),
+                 ('Note', {'fields': ('note',)})
+                 )
 
 admin.site.register(BPAUser, BPAUserAdmin)
 
