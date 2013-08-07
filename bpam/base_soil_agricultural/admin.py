4f19e62fe0243eb734c5b47130b887c0608ec684
Thys Meintjes <tmeintjes@ccg.murdoch.edu.au>
2013-08-07 16:13:55 +0800

    Soil Sample Admin finessed

--- bpam/base_soil_agricultural/admin.py
+++ bpam/base_soil_agricultural/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from .models import SoilSample
 
-admin.site.register(SoilSample)
+class SampleAdmin(admin.ModelAdmin):
+    list_display = ('bpa_id', 'name', 'note')
+
+admin.site.register(SoilSample, SampleAdmin)
 
     
     
