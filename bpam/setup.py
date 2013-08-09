e543a25a27ea8cd3b58f5fcbb9f5144562a49212
Rodney Lorrimar <rlorrimar@ccg.murdoch.edu.au>
2013-08-09 09:45:15 +0800

    setup.py: "apps" was missing from packages list
    Change it to use the setuptools package finder.

--- bpam/setup.py
+++ bpam/setup.py
@@ -1,8 +1,12 @@
 import setuptools
-from setuptools import setup
+from setuptools import setup, find_packages
 import os.path
 import re
 from pip.req import parse_requirements
+
+def list_requirements(name):
+    return [str(ir.req) for ir in
+            parse_requirements(os.path.join("..", "requirements", name))]
 
 setup(
     name='bpam',
@@ -11,23 +15,9 @@ setup(
     author='Centre for Comparative Genomics',
     author_email='web@ccg.murdoch.edu.au',
     license="gpl3",
-    packages=[
-        'bpam',
-        'bpam.settings',
-        'apps.bpaauth',
-        'apps.common',
-        'apps.common.management',
-        'apps.common.management.commands',
-        'apps.coral',
-        'apps.melanoma',
-        'apps.base_soil_agricultural',
-        'apps.base_soil_environmental',
-        'apps.wheat_cultivars',
-        'apps.wheat_fungal_pathogens',
-        'scripts',
-        ],
+    packages=find_packages(),
     package_data={
         "bpametadata": [ "templates/admin/base_site.html" ],
         },
-    install_requires=[str(ir.req) for ir in parse_requirements('../requirements/dev.txt')],
+    install_requires=list_requirements("dev.txt"),
     )
