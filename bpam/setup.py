f5ae44496c608deb8308005d536b134441347674
Maciej Radochonski <mradochonski@ccg.murdoch.edu.au>
2013-09-19 09:27:50 +0800

    Remove tendo as its not used in web app

--- bpam/setup.py
+++ bpam/setup.py
@@ -38,7 +38,6 @@ setup(
         "django-bootstrap3==0.0.6",
         "mimeparse==0.1.3",
         "requests==1.2.3",
-        "tendo==0.2.4",
         'ccg-extras==0.1.6',
         'python-memcached==1.48',
         ],
