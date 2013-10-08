3e1a473a60ba430b301da5cc92b655112f911ad3
Thys Meintjes <tmeintjes@ccg.murdoch.edu.au>
2013-10-08 16:39:55 +0800

    Expanded reflow

--- tools/BASE/base_reflow/reflow.py
+++ tools/BASE/base_reflow/reflow.py
@@ -5,10 +5,14 @@
 This tool takes the current base format document and re-flows it to a more normalised version.
 
 Usage:
-  reflow.py [options] INPUT_FILE OUTPUT_FILE
+  reflow.py [options] INPUT_FILE [OUTPUT_FILE_PREFIX]
 
 Options:
-  -v, --verbose    Verbose mode.
+  -v, --verbose       Verbose mode.
+  -s, --get-samples   Get samples.
+  -c, --get-analysis  Get chemical analysis.
+  -s, --get-sites     Get sites.
+  -a, --get-all       Get everything.
 
 """
 
@@ -16,20 +20,40 @@ from unipath import Path
 import logging
 import sys
 import csv
+import pprint
+import time
 from collections import namedtuple
 from docopt import docopt
-
-logging.basicConfig(level=logging.INFO)
+from dateutil.parser import parse as date_parse
 
 __author__ = 'ccg'
 __version__ = '0.1'
+DEFAULT_OUTPUT_FILE_PREFIX = "base"
+
+logging.basicConfig()
+logger = logging.getLogger("baseparser")
+logger.setLevel(level=logging.INFO)
+
+
+def parse_date(str):
+    """
+    Try to get a valid date from the passed in str
+    """
+    date = None
+    err = None
+    try:
+        date = date_parse(str)
+    except ValueError, e:
+        err = e
+    return date, err
+
 
 # maps spreadsheet column name with attribute names, validator and converter functions, if any
 ColumnSpec = namedtuple('ColumnSpec', 'name validator converter')
 COLUMNS = {'Sample#': ColumnSpec('bpa_id', None, None),
-           'Depth (cm)': ColumnSpec('dept', None, None),
+           'Depth (cm)': ColumnSpec('depth', None, None),
            'Site / NP': ColumnSpec('site_name', None, None),
-           'Date Sampled': ColumnSpec('sample_date', None, None),
+           'Date Sampled': ColumnSpec('sample_date', None, parse_date),
            'Soil moisture (%)': ColumnSpec('moisture', None, None),
            'color': ColumnSpec('color', None, None),
            'Gravel (%)': ColumnSpec('gravel', None, None),
@@ -79,32 +103,99 @@ for c_name, col_spec in COLUMNS.items():
     ATTRIBUTES[col_spec.name] = AttributeSpec(c_name, col_spec.validator, col_spec.converter)
 
 
-def parse_to_named_tuple(type_name, reader, fieldspec):
-    """
-    Pack the csv data into a type
-    """
+class Helper(object):
+    field_spec = []
 
-    typ = namedtuple(type_name, fieldspec)
-    for attribute in fieldspec:
+    @classmethod
+    def get_date_str(cls, date):
+        if date is not None:
+            return date.strftime("%d/%m/%Y")
+        else:
+            return ''
+
+    @classmethod
+    def get_header(cls):
+        header = []
+        for atr in cls.field_spec:
+            header.append(ATTRIBUTES[atr].column_name)
+        return header
+
+    @classmethod
+    def unpack(cls, tup_name, reader):
+        """
+        Unpack the csv file to the named tuple type
+        """
+        named_tup = namedtuple(tup_name, ['row'] + cls.field_spec)
+
+        for idx, row in enumerate(reader):
+            tpl = [idx]
+            for field in cls.field_spec:
+                spec = ATTRIBUTES[field]
+                val = row[spec.column_name].decode('utf-8').strip()
+
+                converter_func = spec.converter
+                validator_func = spec.validator
+
+                if converter_func is not None:
+                    val, err = converter_func(val)
+                    if err is not None:
+                        logger.error(err)
+
+                if validator_func is not None:
+                    val, err = validator_func(val)
+                    if err is not None:
+                        logger.error(err)
+
+                tpl.append(val)
+            yield named_tup(*tpl)
 
 
+class SampleHelper(Helper):
 
-def get_samples(reader):
-    """
-    Unpack the samples
-    """
+    field_spec = ['bpa_id', 'depth', 'sample_date']
 
-    field_spec = ['bpaid', 'dept', 'sample_date']
-    parse_to_named_tuple('Sample', reader, field_spec)
+    @classmethod
+    def write_samples(cls, args, samples):
+        """
+        Write list of sample objects to csv file
+        """
+        file_name = args['OUTPUT_FILE_PREFIX'] + '_samples.csv'
+        with open(file_name, 'wb') as csvfile:
+            writer = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            writer.writerow(cls.get_header())
+            for sample in samples:
+                formatted = [sample.bpa_id, sample.depth, cls.get_date_str(sample.sample_date)]
+                writer.writerow(formatted)  # don't want row in file
+
+    @classmethod
+    def get_samples(cls, args):
+        """
+        Parse samples
+        """
+
+        in_file = Path(args['INPUT_FILE'])
+        with open(in_file) as fd:
+            reader = csv.DictReader(fd)
+            cls.write_samples(args, cls.unpack('Sample', reader))
+
+
+class SiteHelper(Helper):
+    @classmethod
+    def get_sites(cls, args):
+        in_file = Path(args['INPUT_FILE'])
+        with open(in_file) as fd:
+            reader = csv.DictReader(fd)
+
 
 
 def main(args):
-    in_file = Path(args['INPUT_FILE'])
-    with open(in_file) as fd:
-        reader = csv.DictReader(fd)
-        samples = get_samples(reader)
+    if args['--get-sites'] is not None:
+        site_helper = SiteHelper()
+        site_helper.get_sites(args)
 
-
+    if args['--get-samples'] is not None:
+        sample_helper = SampleHelper()
+        sample_helper.get_samples(args)
 
 
 def args_check(args):
@@ -119,9 +210,13 @@ def args_check(args):
 if __name__ == '__main__':
     args = docopt(__doc__, version=__version__)
 
+    if args['OUTPUT_FILE_PREFIX'] is None:
+        args['OUTPUT_FILE_PREFIX'] = DEFAULT_OUTPUT_FILE_PREFIX
+
+
     if args['--verbose']:
-        logging.basicConfig(level=logging.DEBUG)
-        logging.info(args)
+        logger.setLevel(level=logging.DEBUG)
+        logger.info(pprint.pprint(args))
 
     args_check(args)
     main(args)
