2c90d19b2de7f2dc4b50a1958c87a2de617cb4a6
Thys Meintjes <tmeintjes@ccg.murdoch.edu.au>
2013-10-11 16:09:44 +0800

    Added search facilities to common BPA ID Admin

--- bpam/apps/common/admin.py
+++ bpam/apps/common/admin.py
@@ -29,6 +29,8 @@ class BPAProjectAdmin(admin.ModelAdmin):
 class BPAUniqueIDAdmin(admin.ModelAdmin):
     fields = (('bpa_id', 'project'), 'note')
     list_display = ('bpa_id', 'project', 'note')
+    search_fields = ('bpa_id', 'project__name', 'note')
+
 
 class FacilityAdmin(admin.ModelAdmin):
     fields = (('name', 'service'), 'note')
