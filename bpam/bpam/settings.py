059293902bad9e1b53d6e3b08f5430fab6afa80d
Thys Meintjes <tmeintjes@ccg.murdoch.edu.au>
2013-10-24 10:33:01 +0800

    Upped to version 1.0.4

--- bpam/bpam/settings.py
+++ bpam/bpam/settings.py
@@ -4,7 +4,7 @@ import os
 from unipath import Path
 from django.core.exceptions import ImproperlyConfigured
 
-BPA_VERSION = '1.0.3'
+BPA_VERSION = '1.0.4'
 
 WEBAPP_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
