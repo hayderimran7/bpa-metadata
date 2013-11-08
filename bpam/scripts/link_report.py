7bbe32f59386c7dc1815bf864397226c246773a7
Thys Meintjes <tmeintjes@ccg.murdoch.edu.au>
2013-11-08 14:24:08 +0800

    BRLOPS-196 Add issue column

--- bpam/scripts/link_report.py
+++ bpam/scripts/link_report.py
@@ -14,16 +14,18 @@ from apps.melanoma.models import MelanomaSequenceFile
 MISSING_REPORT_NAME = 'missing_melanoma.xlsx'
 
 
-def make_report(missing):
-    logger.info('{0} missing Melanoma Sequence files'.format(len(missing)))
+def make_report(not_ok):
+    logger.info('{0} Melanoma Sequence files with issues'.format(len(not_ok)))
     book = xlwt.Workbook()
-    sheet = book.add_sheet('Missing Melanoma Sequence Files')
+    sheet = book.add_sheet('Melanoma Files with issues')
     sheet.write(0, 0, 'BPA ID')
     sheet.write(0, 1, 'File Name')
+    sheet.write(0, 2, 'Issue')
 
-    for i, f in enumerate(missing):
+    for i, f in enumerate(not_ok):
         sheet.write(i + 1, 0, f.sample.bpa_id.bpa_id)
         sheet.write(i + 1, 1, f.filename)
+        sheet.write(i + 1, 2, f.ingest_issue)
 
     book.save(MISSING_REPORT_NAME)
     logger.info('Wrote report {0}'.format(MISSING_REPORT_NAME))
@@ -31,5 +33,5 @@ def make_report(missing):
 
 def run():
     # melanoma
-    missing = [f for f in MelanomaSequenceFile.objects.all() if not f.link_ok()]
-    make_report(missing)
+    not_ok = [f for f in MelanomaSequenceFile.objects.all() if not f.link_ok()]
+    make_report(not_ok)
