4c5e76dc2d43266445f08e05890c0049f8d5974b
Thys Meintjes <tmeintjes@ccg.murdoch.edu.au>
2013-11-28 22:12:04 +0800

    Better pattern handling

--- bpam/apps/melanoma/urls.py
+++ bpam/apps/melanoma/urls.py
@@ -1,10 +1,10 @@
-
-from django.conf.urls import patterns, include, url
-from django.contrib.auth.decorators import login_required, permission_required
+from django.conf.urls import patterns, url
+from django.contrib.auth.decorators import login_required
 
 from apps.melanoma.views import MelanomaSequenceFileListView, search_view
 
-urlpatterns = patterns('',
-    url(r'^melanoma/?$', login_required(MelanomaSequenceFileListView.as_view()), name="melanoma-list"),
-    url(r'^melanoma/search/(.*)$', search_view, name="melanoma-search"),
+urlpatterns = patterns(
+    '',
+    url(r'^$', login_required(MelanomaSequenceFileListView.as_view()), name="melanoma-list"),
+    url(r'search/(.*)$', search_view, name="melanoma-search"),
 )
