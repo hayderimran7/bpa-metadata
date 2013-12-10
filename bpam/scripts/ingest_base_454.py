3f755f958a37541ef5b6f35d9f7c974262f4bb82
Thys Meintjes <tmeintjes@ccg.murdoch.edu.au>
2013-12-10 11:10:36 +0800

    column spec first cut

--- bpam/scripts/ingest_base_454.py
+++ bpam/scripts/ingest_base_454.py
@@ -30,42 +30,42 @@ def get_data(spreadsheet_file):
     The data sets is relatively small, so make a in-memory copy to simplify some operations.
     """
 
-    fieldnames = ['bpa_id',
-                  'sample_id',
-                  'aurora_purified',
-                  'dna_storage_nunc_plate',
-                  'dna_storage_nunc_tube',
-                  'dna_storage_nunc_well_location',
-                  'agrf_batch_number',
-                  'submitter_name',
-                  'date_received',
+    fieldnames = [('bpa_id', '', None),
+                  ('sample_id', '', None),
+                  ('aurora_purified', '', None),
+                  ('dna_storage_nunc_plate', '', None),
+                  ('dna_storage_nunc_tube', '', None),
+                  ('dna_storage_nunc_well_location', '', None),
+                  ('agrf_batch_number', '', None),
+                  ('submitter_name', '', None),
+                  ('date_received', '', None),
                   # Adelaide extraction
-                  'adelaide_extraction_sample_weight',  # mg
-                  'adelaide_fluorimetry',  # ng/uL gDNA'
-                  'adelaide_pcr_inhibition',
-                  'adelaide_pcr1',
-                  'adelaide_pcr2',
-                  'adelaide_date_shipped_to_agrf_454',
-                  'adelaide_date_shipped_to_agrf_miseq',
-                  'adelaide_date_shipped_to_ramacciotitti',
+                  ('adelaide_extraction_sample_weight', '', None),  # mg
+                  ('adelaide_fluorimetry', '', None),  # ng/uL gDNA'
+                  ('adelaide_pcr_inhibition', '', None),
+                  ('adelaide_pcr1', '', None),
+                  ('adelaide_pcr2', '', None),
+                  ('adelaide_date_shipped_to_agrf_454', '', None),
+                  ('adelaide_date_shipped_to_agrf_miseq', '', None),
+                  ('adelaide_date_shipped_to_ramacciotitti', '', None),
                   # Brisbane 454
-                  'brisbane_16s_mid',
-                  'brisbane_its_mid',
-                  'brisbane_16s_pcr1',
-                  'brisbane_16s_pcr2',
-                  'brisbane_16s_pcr3',
-                  'brisbane_its_pcr1_neat',
-                  'brisbane_its_pcr2_1_10',
-                  'brisbane_its_pcr3_fusion',
-                  'brisbane_fluorimetry_16s',  # ng/uL 16S
-                  'brisbane_fluorimetry_its',  # ng/uL 16S
-                  'brisbane_16s_qpcr',
-                  'brisbane_its_qpcr',
-                  'brisbane_i6s_pooled',
-                  'brisbane_its_pooled',
-                  'brisbane_16s_reads',
-                  'brisbane_itss_reads',
-                  'note',
+                  ('brisbane_16s_mid', '', None),
+                  ('brisbane_its_mid', '', None),
+                  ('brisbane_16s_pcr1', '', None),
+                  ('brisbane_16s_pcr2', '', None),
+                  ('brisbane_16s_pcr3', '', None),
+                  ('brisbane_its_pcr1_neat', '', None),
+                  ('brisbane_its_pcr2_1_10', '', None),
+                  ('brisbane_its_pcr3_fusion', '', None),
+                  ('brisbane_fluorimetry_16s', '', None),  # ng/uL 16S
+                  ('brisbane_fluorimetry_its', '', None),  # ng/uL 16S
+                  ('brisbane_16s_qpcr', '', None),
+                  ('brisbane_its_qpcr', '', None),
+                  ('brisbane_i6s_pooled', '', None),
+                  ('brisbane_its_pooled', '', None),
+                  ('brisbane_16s_reads', '', None),
+                  ('brisbane_itss_reads', '', None),
+                  ('note', '', None),
                   ]
 
     wb = xlrd.open_workbook(spreadsheet_file)
