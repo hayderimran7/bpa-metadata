467496833c3d4fdd90b1dadbc34cc45bba53486f
Thys Meintjes <tmeintjes@ccg.murdoch.edu.au>
2013-12-16 16:21:06 +0800

    Better parsing error handling.

--- bpam/scripts/row_tools.py
+++ bpam/scripts/row_tools.py
@@ -15,6 +15,16 @@ logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
 
+class ColumnNotFoundException(Exception):
+    column_name = 'Not Set'
+
+    def __init__(self, column_name):
+        self.column_name = column_name
+
+    def __str__(self):
+        return 'Column {0} not found'.format(self.column_name)
+
+
 def _stringify(s):
     if isinstance(s, str):
         return str(s.decode('utf8'))
@@ -60,18 +70,20 @@ def parse_to_named_tuple(typname, reader, header, fieldspec):
 
     typ = namedtuple(typname, ['row'] + [t[0] for t in fieldspec])
     lookup = []
-    fns = [t[2] for t in fieldspec]
+    functions = [t[2] for t in fieldspec]
     for _, field_name, _ in fieldspec:
         idx = header.index(field_name)
         if idx == -1:
-            logger.error('Expected column name {0} not found, quitting'.format(field_name))
-            os.exit(1)
+            raise ColumnNotFoundException(field_name)
 
         assert (idx != -1)
         lookup.append(idx)
+
     for idx, row in enumerate(reader):
         tpl = [idx]
-        for fn, i in zip(fns, lookup):
+        for fn, i in zip(functions, lookup):
+            # if the column is a date, be nice to it
+            if
             val = row[i].decode('utf-8').strip()
             if fn is not None:
                 val = fn(val)
