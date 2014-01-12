4224f81e661ad0f6a92c53ffbff1eca3822ec80f
Thys Meintjes <tmeintjes@ccg.murdoch.edu.au>
2014-01-12 16:06:37 +0800

    Ordered up form

--- bpam/apps/BASE/admin.py
+++ bpam/apps/BASE/admin.py
@@ -15,6 +15,51 @@ class Sample454Admin(admin.ModelAdmin):
     search_fields = ('bpa_id__bpa_id', 'sample_id',)
     list_filter = ('bpa_id__bpa_id',)
 
+    fieldsets = [
+        (None,
+         {'fields':
+              ('bpa_id', 'sample_id', 'aurora_purified', 'agrf_batch_number', 'submitter', 'date_received')
+         }),
+        ('DNA Storage',
+         {'fields':
+              ('dna_storage_nunc_plate', 'dna_storage_nunc_tube', 'dna_storage_nunc_well_location', )
+         }),
+        ('AGRF Adelaide Extraction',
+         {'fields':
+              ('adelaide_extraction_sample_weight',
+               'adelaide_fluorimetry',
+               'adelaide_pcr_inhibition',
+               'adelaide_pcr1',
+               'adelaide_pcr2',
+               'adelaide_date_shipped_to_agrf_454',
+               'adelaide_date_shipped_to_agrf_miseq',
+               'adelaide_date_shipped_to_ramacciotti')
+         }),
+        ('Brisbane 454',
+         {'fields':
+              ('brisbane_16s_mid',
+               'brisbane_its_mid',
+               'brisbane_16s_pcr1',
+               'brisbane_16s_pcr2',
+               'brisbane_16s_pcr3',
+               'brisbane_its_pcr1_neat',
+               'brisbane_its_pcr2_1_10',
+               'brisbane_its_pcr3_fusion',
+               'brisbane_fluorimetry_16s',
+               'brisbane_fluorimetry_its',
+               'brisbane_16s_qpcr',
+               'brisbane_its_qpcr',
+               'brisbane_i6s_pooled',
+               'brisbane_its_pooled',
+               'brisbane_16s_reads',
+               'brisbane_its_reads',)
+         }),
+        ('Note', {'fields': ('note', )})
+    ]
+
+
+admin.site.register(Sample454, Sample454Admin)
+
 
 class ChemicalAnalysisAdmin(admin.ModelAdmin):
     list_display = ('bpa_id', 'lab_name_id', 'depth', 'colour', 'gravel', 'texture')
@@ -50,7 +95,7 @@ admin.site.register(TillageType)
 admin.site.register(BroadVegetationType)
 admin.site.register(GeneralEcologicalZone)
 admin.site.register(SoilMetagenomicsSample, SampleAdmin)
-admin.site.register(Sample454, Sample454Admin)
+
 admin.site.register(LandUse, LandUseAdmin)
 admin.site.register(SiteOwner)
 admin.site.register(CollectionSiteHistory)
