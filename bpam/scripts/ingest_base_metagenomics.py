ebe5f9640f58723c377eb3255dc22d266d87a296
Thys Meintjes <tmeintjes@ccg.murdoch.edu.au>
2014-02-23 23:31:45 +0800

    Common logging. Make use of common utils. Looser coupling.

--- bpam/scripts/ingest_base_metagenomics.py
+++ bpam/scripts/ingest_base_metagenomics.py
@@ -1,56 +1,48 @@
-import logging
-import pprint
-import sys
 from unipath import Path
 
 from libs.excel_wrapper import ExcelWrapper
 from libs import bpa_id_utils
 from libs import ingest_utils
+from libs import logger_utils
+from apps.base.models.metagenomics import MetagenomicsSample, MetagenomicsSequenceFile
 
-from apps.base.models.metagenomics import SoilMetagenomicsSample, MetagenomicsSequenceFile
 
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger('base Metagenomics')
+logger = logger_utils.get_logger('BASE Metagenomics')
 
 DATA_DIR = Path(Path(__file__).ancestor(3), "data/base/")
 DEFAULT_SPREADSHEET_FILE = Path(DATA_DIR, 'metagenomics')
 
 BPA_ID = "102.100.100"
-BASE_DESCRIPTION = 'base'
+BASE_DESCRIPTION = 'BASE'
 
 
-def get_bpa_id(t):
-    if bpa_id_utils.is_good_bpa_id(t.bpa_id):
-        return bpa_id_utils.get_bpa_id(t.bpa_id, BASE_DESCRIPTION, note='base Metagenomics Sample')
-    else:
-        return None
-
-
-def get_sample(t):
+def get_sample(e):
     """
     Get the Sample by bpa_id
     """
 
-    bpa_id = get_bpa_id(t)
+    bpa_id = bpa_id_utils.get_bpa_id(e.bpa_id, BASE_DESCRIPTION, note='BASE Metagenomics Sample')
     if bpa_id is None:
         return None
 
     try:
-        sample = SoilMetagenomicsSample.objects.get(bpa_id=bpa_id)
-    except SoilMetagenomicsSample.DoesNotExist:
+        sample = MetagenomicsSample.objects.get(bpa_id__bpa_id=bpa_id)
+    except MetagenomicsSample.DoesNotExist:
         logger.debug('Adding Metagenomics Sample ' + bpa_id.bpa_id)
+        sample = MetagenomicsSample(bpa_id=bpa_id)
 
     # always update
-    sample = SoilMetagenomicsSample(bpa_id=bpa_id)
-    sample.name = t.sample_id
-    sample.dna_extraction_protocol = t.library_protocol
-    sample.requested_sequence_coverage = t.library_construction
-    sample.collection_date = t.date_received
+    sample.name = e.sample_id
+    sample.dna_extraction_protocol = e.library_protocol
+    sample.requested_sequence_coverage = e.library_construction
+    sample.collection_date = e.date_received
+    sample.debug_note = ingest_utils.pretty_print_namedtuple(e)
+    sample.save()
 
     return sample
 
 
-def ingest(file_name):
+def get_data(file_name):
     """
     The data sets is relatively small, so make a in-memory copy to simplify some operations.
     """
@@ -95,19 +87,21 @@ def ingest(file_name):
 
     wrapper = ExcelWrapper(field_spec,
                            file_name,
-                           sheet_name='base Metagenomics',
+                           sheet_name='BASE Metagenomics',
                            header_length=1,
                            column_name_row_index=0)
-    for file_row in wrapper.get_all():
-        # ID
-        bpa_id = get_bpa_id(file_row)
-        if bpa_id is None:
-            logger.warning('BPA ID {0} does not look like a proper BPA ID ignoring'.format(file_row.bpa_id))
-            continue
 
-        sample = get_sample(bpa_id)
+    return wrapper.get_all()
+
+
+def add_sequence_files(data):
+    """
+    Add sequence files
+    """
+    for file_row in data:
+        sample = get_sample(file_row)
         if sample is None:
-            logger.error('Could not add sample ' + str(sample))
+            logger.warning('Could not add sample {0}'.format(sample))
 
         sequence_file = MetagenomicsSequenceFile()
         sequence_file.sample = sample
@@ -116,15 +110,9 @@ def ingest(file_name):
         sequence_file.index_number = file_row.index
         sequence_file.lane_number = file_row.lane_number
         sequence_file.date_received_from_sequencing_facility = file_row.date_received
-        sequence_file.analysed = file_row.analysed
         sequence_file.note = file_row.comments
 
-        try:
-            sequence_file.save()
-        except Exception, e:
-            pprint.pprint(e)
-            pprint.pprint(sequence_file)
-            sys.exit(1)
+        sequence_file.save()
 
 
 def run(file_name=DEFAULT_SPREADSHEET_FILE):
@@ -133,6 +121,7 @@ def run(file_name=DEFAULT_SPREADSHEET_FILE):
     vpython-bpam manage.py runscript ingest_base_454 --script-args Melanoma_study_metadata.xlsx
     """
 
-    ingest(file_name)
+    data = list(get_data(file_name))
+    add_sequence_files(data)
 
 
