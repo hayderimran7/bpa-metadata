6de8370388203184ee550205bd59091424efa0e1
Thys Meintjes <tmeintjes@ccg.murdoch.edu.au>
2014-02-23 23:29:52 +0800

    Factored out url verification code to common sequence file model.

--- bpam/apps/gbr/models.py
+++ bpam/apps/gbr/models.py
@@ -1,11 +1,8 @@
-import urlparse
-import urllib
-
 from django.db import models
 from django.conf import settings
 from django.utils.translation import ugettext_lazy as _
 
-from apps.common.models import Protocol, Sample, Run, SequenceFile, Organism, URLVerification, DebugNote
+from apps.common.models import Protocol, Sample, Run, SequenceFile, Organism, DebugNote
 
 
 class CollectionEvent(models.Model):
@@ -71,26 +68,7 @@ class GBRSequenceFile(SequenceFile):
 
     sample = models.ForeignKey(GBRSample)
     run = models.ForeignKey(GBRRun)
-    url_verification = models.OneToOneField(URLVerification, null=True)
+    project_name = 'gbr'
 
     def __unicode__(self):
         return u'Run {0} for {1}'.format(self.run, self.filename)
-
-    def link_ok(self):
-        if self.url_verification is not None:
-            return self.url_verification.status_ok
-        else:
-            return False
-
-    # FIXME move this into sequencefile, code duplicated with MelanomaSequenceFile
-    def get_url(self):
-        bpa_id = self.sample.bpa_id.bpa_id.replace('/', '.')
-        uj = urlparse.urljoin
-        uq = urllib.quote
-        return uj(settings.BPA_BASE_URL, "GBR/%s/%s/%s" % (
-            uq(bpa_id),
-            uq(self.run.flow_cell_id),
-            uq(self.filename)))
-
-    url = property(get_url)
-
