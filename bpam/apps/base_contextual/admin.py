2a9ebbf0124c56c183f53e6e89b2e5e186609e2a
Thys Meintjes <tmeintjes@ccg.murdoch.edu.au>
2014-03-13 19:28:17 +0800

    removed duplications

--- bpam/apps/base_contextual/admin.py
+++ bpam/apps/base_contextual/admin.py
@@ -84,8 +84,6 @@ class CollectionSiteAdmin(admin.ModelAdmin):
                 'date_sampled': SuitDateWidget,
                 'profile_position': LinkedSelect,
                 'drainage_classification': LinkedSelect,
-                'soil_type_australian_classification': LinkedSelect,
-                'soil_type_fao_classification': LinkedSelect,
             }
 
     form = CollectionForm
