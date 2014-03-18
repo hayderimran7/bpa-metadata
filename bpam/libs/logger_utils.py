53fccf9bca1da74e660210e6183a56b32fb75605
Thys Meintjes <tmeintjes@ccg.murdoch.edu.au>
2014-03-18 11:06:18 +0800

    Adding rainbow to django config made this redundant.

--- bpam/libs/logger_utils.py
+++ bpam/libs/logger_utils.py
@@ -1,11 +1,10 @@
-import sys
+# this module was more useful before I added rainbow to django
+
 import logging
-from rainbow_logging_handler import RainbowLoggingHandler
 
 
 def get_logger(name):
     logger = logging.getLogger(name)
     logger.setLevel(logging.DEBUG)
-    logger.addHandler(RainbowLoggingHandler(sys.stderr, color_funcName=('black', 'yellow', True)))
 
     return logger
