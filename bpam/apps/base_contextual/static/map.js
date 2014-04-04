0e33e5112970a5cfc39cdc1ba2fc2705ba64f1ee
Rodney Lorrimar <rlorrimar@ccg.murdoch.edu.au>
2014-04-04 15:20:44 +0800

    BM-105 Collection sites map: fix popup layout issues

--- bpam/apps/base_contextual/static/map.js
+++ bpam/apps/base_contextual/static/map.js
@@ -3,20 +3,24 @@ var BPAM = (function() {
 
   return {
     map_init_list: function(map, options) {
-        $("#sitelist tbody tr").each(function(index, el) {
-          var get = function(cls) { return $(el).find("td." + cls).text(); };
-          var marker = L.marker([get("lat"), get("lng")], {
-            title: get("name")
-          });
-          var href = $(el).find("td.name a").attr("href");
-          var content = $("<div class='site-popup-table'>Loading...</div>");
-          marker.addTo(map);
-          marker.bindPopup(content.wrap("<div/>").parent()
-                           .prepend("<h3>" + get("name") + "</h3>")[0]);
-          marker.on("popupopen", function(ev) {
-            content.load(href + " table#collectionsite");
+      $("#sitelist tbody tr").each(function(index, el) {
+        var get = function(cls) { return $(el).find("td." + cls).text(); };
+        var marker = L.marker([get("lat"), get("lng")], {
+          title: get("name")
+        });
+        var href = $(el).find("td.name a").attr("href");
+        var content = $("<div class='site-popup-table'>Loading...</div>");
+        marker.addTo(map);
+        marker.bindPopup(content.wrap("<div/>").parent()
+                         .prepend("<h3>" + get("name") + "</h3>")[0]);
+        marker.on("popupopen", function(ev) {
+          content.load(href + " table#collectionsite", function() {
+            // after loading content, update popup so it gets the
+            // right size and so map pans to make entire popup visible
+            ev.popup.update();
           });
         });
+      });
 
       $(window).resize(function() {
         var targets = $("#site_map, #sitelist-container");
