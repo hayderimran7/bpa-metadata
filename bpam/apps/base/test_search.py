886eaa06b8c4dff12494e6df512be1ddcc9258ea
Lee Render <lrender@ccg.murdoch.edu.au>
2014-04-17 10:46:29 +0800

    BM-103 Fixed breaking search tests after refactor

--- bpam/apps/base/test_search.py
+++ bpam/apps/base/test_search.py
@@ -11,17 +11,17 @@ from apps.base_contextual.models import *
 class SearchTestCase(TestCase):
     def setUp(self):
         super(SearchTestCase, self).setUp()
-        self.base_sample = mommy.make(BaseSample)
-        self.bpa_id = self.base_sample.bpa_id
+        self.BASE_sample = mommy.make(BASESample)
+        self.bpa_id = self.BASE_sample.bpa_id
         self.collection_site = mommy.make(CollectionSite)
 
-        self.collection_sample = mommy.make(CollectionSample, bpa_id=self.bpa_id, site=self.collection_site)
+        self.collection_sample = mommy.make(SampleContext, bpa_id=self.bpa_id, site=self.collection_site)
         self.chemical_analysis = mommy.make(ChemicalAnalysis, bpa_id=self.bpa_id)
         self.otu = mommy.make(OperationalTaxonomicUnit)
-        self.sample_otu = mommy.make(SampleOTU, sample=self.base_sample, otu=self.otu)
+        self.sample_otu = mommy.make(SampleOTU, sample=self.BASE_sample, otu=self.otu)
 
     def testTaxonomicFiltering(self):
-        # find CollectionSamples with chemical composition boron hot cacl2 = 0.003 with species starting with foobar
+        # find SampleContexts with chemical composition boron hot cacl2 = 0.003 with species starting with foobar
         self.otu.species = "foobar baz"
         self.otu.save()
         self.chemical_analysis.boron_hot_cacl2 = 0.003
@@ -33,7 +33,7 @@ class SearchTestCase(TestCase):
         samples = searcher.get_matching_samples()
         assert len(samples) == 1, "Expected one matching sample - got %s" % len(samples)
         found_sample = samples[0]
-        assert found_sample.__class__.__name__ == "CollectionSample"
+        assert found_sample.__class__.__name__ == "SampleContext"
 
     def testRangeSearch(self):
         self.chemical_analysis.boron_hot_cacl2 = 0.5
@@ -49,7 +49,7 @@ class SearchTestCase(TestCase):
         samples = searcher.get_matching_samples()
         assert len(samples) == 1, "Expected one matching sample - got %s" % len(samples)
         found_sample = samples[0]
-        assert found_sample.__class__.__name__ == "CollectionSample"
+        assert found_sample.__class__.__name__ == "SampleContext"
 
     def testSearchOnSiteSubObjectField(self):
         self.collection_site.elevation = 23
@@ -65,7 +65,7 @@ class SearchTestCase(TestCase):
         samples = searcher.get_matching_samples()
         assert len(samples) == 1, "Expected one matching sample - got %s" % len(samples)
         found_sample = samples[0]
-        assert found_sample.__class__.__name__ == "CollectionSample"
+        assert found_sample.__class__.__name__ == "SampleContext"
 
     def testFailingSearchOnSiteSubObjectField(self):
         self.collection_site.elevation = 25
