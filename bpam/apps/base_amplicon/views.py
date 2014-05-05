9422fbcf060ca2d139f437864229712b9faad2d2
Thys Meintjes <tmeintjes@ccg.murdoch.edu.au>
2014-05-05 22:42:20 +0800

    Amplicon user View

--- bpam/apps/base_amplicon/views.py
+++ bpam/apps/base_amplicon/views.py
@@ -14,3 +14,7 @@ class MetadataView(DetailView):
     context_object_name = 'metadata'
     template_name = 'base_amplicon/metadata_detail.html'
 
+    def get_context_data(self, **kwargs):
+        context = super(MetadataView, self).get_context_data(**kwargs)
+        context['sequencefiles'] = AmpliconSequenceFile.objects.filter(sample__bpa_id=context['metadata'].bpa_id)
+        return context
