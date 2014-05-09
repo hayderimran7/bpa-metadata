23cb0ca6fa310a5f54bb5df9dba721214b7fa293
Lee Render <lrender@ccg.murdoch.edu.au>
2014-05-09 14:27:23 +0800

    BM-113 Replaced simple search unit tests with complex search unit tests.

--- bpam/apps/base/test_search.py
+++ bpam/apps/base/test_search.py
@@ -6,94 +6,180 @@ from apps.common.models import *
 from apps.base.models import *
 from apps.base_otu.models import *
 from apps.base_contextual.models import *
+from apps.base_metagenomics.models import *
+
+
+class SearchBuilder(object):
+    # mocks parameters from the view
+    def __init__(self):
+        self.parameters = {}
+        self.parameters["search_all"] = "No"
+        self.parameters["search_terms"] = []
+        self.search_all(False)
+
+    def and_search(self):
+        self.parameters["search_operator"] = 'and'
+
+    def or_search(self):
+        self.parameters["search_operator"] = 'or'
+
+    def search_all(self, search_all=True):
+        if search_all:
+            self.parameters["search_all"] = "search_all"
+        else:
+            self.parameters["search_all"] = ""
+
+    def add_search_term(self, field, a, b=None):
+        if b is None:
+            self.parameters["search_terms"].append((field, str(a)))
+        else:
+            # range
+            self.parameters["search_terms"].append((field, (str(a), str(b))))
+
+    def taxonomy_filter(self, kingdom="", phylum="", klass="", order="", family="", genus="", species=""):
+        self.parameters["search_kingdom"] = kingdom
+        self.parameters["search_phylum"] = phylum
+        self.parameters["search_class"] = klass
+        self.parameters["search_order"] = order
+        self.parameters["search_family"] = family
+        self.parameters["search_genus"] = genus
+        self.parameters["search_species"] = species
+
+    def no_taxonomy(self):
+        self.taxonomy_filter()
 
 
 class SearchTestCase(TestCase):
+    def _setUpSample(self, name, elevation, boron_hot_cacl2,otu=None):
+        bpa_id_name = "bpa_id" + str(name)
+        setattr(self, bpa_id_name, mommy.make(BPAUniqueID, bpa_id=str(name)))
+        bpa_id = getattr(self, bpa_id_name)
+        setattr(self, "collection_site" + str(name), mommy.make(CollectionSite, elevation=elevation))
+        site = getattr(self, "collection_site" + str(name))
+        setattr(self, "chemical_analysis" + str(name), mommy.make(ChemicalAnalysis, bpa_id=bpa_id, boron_hot_cacl2=boron_hot_cacl2))
+        setattr(self, "sample_context" + str(name), mommy.make(SampleContext, bpa_id=bpa_id, site=site))
+        setattr(self, "metagenomics_sample" + str(name), mommy.make(MetagenomicsSample, bpa_id=bpa_id))
+        if otu:
+            metagenomics_sample = getattr(self, "metagenomics_sample" + str(name))
+            sample_otu_name = "sample_otu" + str(name)
+            setattr(self, sample_otu_name, mommy.make(SampleOTU, sample=metagenomics_sample, otu=otu, count=1000))
+
     def setUp(self):
         super(SearchTestCase, self).setUp()
-        self.BASE_sample = mommy.make(BASESample)
-        self.bpa_id = self.BASE_sample.bpa_id
-        self.collection_site = mommy.make(CollectionSite)
+        # otus
+        otu1 = OperationalTaxonomicUnit()
+        otu1.name = "otu1"
+        otu1.kingdom = "a"
+        otu1.phylum = "b"
+        otu1.otu_class = "c"
+        otu1.order = "d"
+        otu1.family = "e"
+        otu1.genus = "f"
+        otu1.species = "g"
+        otu1.save()
+        
+        otu2 = OperationalTaxonomicUnit()
+        otu2.name = "otu2"
+        otu2.kingdom = "a"
+        otu2.phylum = "b"
+        otu2.otu_class = "c"
+        otu2.order = "p"
+        otu2.family = "q"
+        otu2.genus = "r"
+        otu2.species = "s"
+        otu2.save()
+        
+        otu3 = OperationalTaxonomicUnit()
+        otu3.name = "otu3"
+        otu3.kingdom = "a"
+        otu3.phylum = "b"
+        otu3.otu_class = "c"
+        otu3.order = "p"
+        otu3.family = "q"
+        otu3.genus = "t"
+        otu3.species = "u"
+        otu3.save()
 
-        self.collection_sample = mommy.make(SampleContext, bpa_id=self.bpa_id, site=self.collection_site)
-        self.chemical_analysis = mommy.make(ChemicalAnalysis, bpa_id=self.bpa_id)
-        self.otu = mommy.make(OperationalTaxonomicUnit)
-        self.sample_otu = mommy.make(SampleOTU, sample=self.BASE_sample, otu=self.otu)
+        # sample 1
+        self._setUpSample("A", 100, 23.67, otu1)
+        # sample 2
+        self._setUpSample("B", 100, 67.0, otu2)
+        # sample 3
+        self._setUpSample("C", 200, 24.67)
+        # sample 4
+        self._setUpSample("D", 1000, -56.90, otu3)
 
-    def testTaxonomicFiltering(self):
-        # find SampleContexts with chemical composition boron hot cacl2 = 0.003 with species starting with foobar
-        self.otu.species = "foobar baz"
-        self.otu.save()
-        self.chemical_analysis.boron_hot_cacl2 = 0.003
-        self.chemical_analysis.save()
+    def testSimpleSingleSearch(self):
+        b = SearchBuilder()
+        b.and_search()
+        b.add_search_term("elevation", 100)
+        b.no_taxonomy()
+        searcher = Searcher(b.parameters)
+        results = searcher.complex_search()
+        assert len(results) == 2, "simple single search failed"
 
-        search_parameters = {"search_field": "boron_hot_cacl2", "search_value": "0.003", "search_species": "foobar*"}
-        searcher = Searcher(search_parameters)
+    def testSimpleRangeSearch(self):
+        b = SearchBuilder()
+        b.and_search()
+        b.no_taxonomy()
+        b.add_search_term("boron_hot_cacl2", 23.0, 25.00)
+        searcher = Searcher(b.parameters)
+        results = searcher.complex_search()
+        assert len(results) == 2, "simple range search failed"
 
-        samples = searcher.get_matching_samples()
-        assert len(samples) == 1, "Expected one matching sample - got %s" % len(samples)
-        found_sample = samples[0]
-        assert found_sample.__class__.__name__ == "BASESample"
+    def testComplexAndSearch(self):
+        b = SearchBuilder()
+        b.and_search()
+        b.no_taxonomy()
+        b.add_search_term("elevation", 100)
+        b.add_search_term("boron_hot_cacl2", 67.0)
+        searcher = Searcher(b.parameters)
+        results = searcher.complex_search()
+        assert len(results) == 1, "complex search and failed"
 
-    def testRangeSearch(self):
-        self.chemical_analysis.boron_hot_cacl2 = 0.5
-        self.chemical_analysis.save()
+    def testComplexOrSearch(self):
+        b = SearchBuilder()
+        b.or_search()
+        b.no_taxonomy()
+        b.add_search_term("elevation", 100)
+        b.add_search_term("boron_hot_cacl2", 24.67)
+        searcher = Searcher(b.parameters)
+        results = searcher.complex_search()
+        assert len(results) == 3, "complex or search failed"
 
-        search_parameters = {"search_range": "boron_hot_cacl2",
-                             "search_field": "",
-                             "search_range_min": "0.2",
-                             "search_range_max": "23.67"
-        }
+    def testTaxonomySearchOnAllSamples1(self):
+        b = SearchBuilder()
+        b.search_all()
+        b.and_search()
+        b.taxonomy_filter(kingdom="a")
+        searcher = Searcher(b.parameters)
+        results = searcher.complex_search()
+        assert len(results) == 3, "taxomic on all failed kingdom"
 
-        searcher = Searcher(search_parameters)
-        samples = searcher.get_matching_samples()
-        assert len(samples) == 1, "Expected one matching sample - got %s" % len(samples)
-        found_sample = samples[0]
-        assert found_sample.__class__.__name__ == "BASESample"
+    def testTaxonomyAndComplexSearch(self):
+        b = SearchBuilder()
+        b.and_search()
+        b.taxonomy_filter(kingdom="a", phylum="b", klass="c")
+        b.add_search_term("elevation", 100)
+        searcher = Searcher(b.parameters)
+        results = searcher.complex_search()
+        print results
+        assert len(results) == 2, "complex taxomic search failed"
 
-    def testSearchOnSiteSubObjectField(self):
-        self.collection_site.elevation = 23
-        self.collection_site.save()
+    def testTaxonomyAndComplexSearch2(self):
+        b = SearchBuilder()
+        b.and_search()
+        b.taxonomy_filter(kingdom="a", phylum="b", klass="dfkdjsfdsfj")
+        b.add_search_term("elevation", 100)
+        searcher = Searcher(b.parameters)
+        results = searcher.complex_search()
+        assert len(results) == 0, "complex taxomic search failed"
 
-        search_parameters = {"search_range": "elevation",
-                             "search_field": "",
-                             "search_range_min": "22",
-                             "search_range_max": "24"
-        }
-
-        searcher = Searcher(search_parameters)
-        samples = searcher.get_matching_samples()
-        assert len(samples) == 1, "Expected one matching sample - got %s" % len(samples)
-        found_sample = samples[0]
-        assert found_sample.__class__.__name__ == "BASESample"
-
-    def testFailingSearchOnSiteSubObjectField(self):
-        self.collection_site.elevation = 25
-        self.collection_site.save()
-
-        search_parameters = {"search_range": "elevation",
-                             "search_field": "",
-                             "search_range_min": "22",
-                             "search_range_max": "24"
-        }
-
-        searcher = Searcher(search_parameters)
-        samples = searcher.get_matching_samples()
-        assert len(samples) == 0, "Expected zero matching samples - got %s" % len(samples)
-
-
-    def testComplexSearch(self):
-        self.chemical_analysis.dtpa_zinc = 23.7
-        self.chemical_analysis.boron_hot_cacl2 = 10.123
-
-
-
-
-
-
-
-
-
-
-
-
+    def testTaxonomyAndComplexSearch3(self):
+        b = SearchBuilder()
+        b.and_search()
+        b.taxonomy_filter(kingdom="a", phylum="b", klass="c")
+        b.add_search_term("boron_hot_cacl2", 23.67)
+        searcher = Searcher(b.parameters)
+        results = searcher.complex_search()
+        assert len(results) == 1, "complex taxomic search failed"
