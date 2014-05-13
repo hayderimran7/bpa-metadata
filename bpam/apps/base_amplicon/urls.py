1ab92137ee61f8c101583f0a0cc65dd8e9d35bed
Thys Meintjes <tmeintjes@ccg.murdoch.edu.au>
2014-05-13 10:03:55 +0800

    BM-123 Added individual Amplicon pages

--- bpam/apps/base_amplicon/urls.py
+++ bpam/apps/base_amplicon/urls.py
@@ -5,11 +5,31 @@ from . import views
 urlpatterns = patterns(
     '',
     url(
-        regex=r'^all_metadata',
-        view=views.MetadataListView.as_view(),
-        name='all_metadata'),
+        regex=r'^$',
+        view=views.IndexView.as_view(),
+        name='index'),
     url(
-        regex=r'^metadata/(?P<pk>.*)/$',
-        view=views.MetadataView.as_view(),
-        name='metadata'),
+        regex=r'^amplicons$',
+        view=views.AmpliconListView.as_view(),
+        name='amplicons'),
+    url(
+        regex=r'^amplicons/16S/$',
+        view=views.Amplicon16SListView.as_view(),
+        name='amplicons_16S'),
+    url(
+        regex=r'^amplicons/18S/$',
+        view=views.Amplicon18SListView.as_view(),
+        name='amplicons_18S'),
+    url(
+        regex=r'^amplicons/ITS/$',
+        view=views.AmpliconITSListView.as_view(),
+        name='amplicons_ITS'),
+    url(
+        regex=r'^amplicons/A16S/$',
+        view=views.AmpliconA16SListView.as_view(),
+        name='amplicons_A16S'),
+    url(
+        regex=r'^amplicon/(?P<pk>.*)/$',
+        view=views.AmpliconDetailView.as_view(),
+        name='amplicon'),
 )
--- bpam/apps/base_amplicon/views.py
+++ bpam/apps/base_amplicon/views.py
@@ -1,22 +1,58 @@
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, TemplateView
 
 from .models import *
 
+class IndexView(TemplateView):
+    template_name = 'base_amplicon/index.html'
 
-class MetadataListView(ListView):
+
+class AmpliconListView(ListView):
     model = AmpliconSequencingMetadata
     context_object_name = 'metadata_list'
     template_name = 'base_amplicon/metadata_list.html'
     # paginate_by = 25
 
 
-class MetadataView(DetailView):
+class Amplicon16SListView(AmpliconListView):
+    def get_context_data(self, **kwargs):
+        context = super(Amplicon16SListView, self).get_context_data(**kwargs)
+        context['target'] = '16S'
+        context['metadata_list'] = AmpliconSequencingMetadata.objects.filter(target='16S')
+        return context
+
+
+class Amplicon18SListView(AmpliconListView):
+    def get_context_data(self, **kwargs):
+        context = super(Amplicon18SListView, self).get_context_data(**kwargs)
+        context['target'] = '18S'
+        context['metadata_list'] = AmpliconSequencingMetadata.objects.filter(target='18S')
+        return context
+
+
+class AmpliconITSListView(AmpliconListView):
+    def get_context_data(self, **kwargs):
+        context = super(AmpliconITSListView, self).get_context_data(**kwargs)
+        context['target'] = 'ITS'
+        context['metadata_list'] = AmpliconSequencingMetadata.objects.filter(target='ITS')
+        return context
+
+class AmpliconA16SListView(AmpliconListView):
+    def get_context_data(self, **kwargs):
+        context = super(AmpliconA16SListView, self).get_context_data(**kwargs)
+        context['target'] = 'A16S'
+        context['metadata_list'] = AmpliconSequencingMetadata.objects.filter(target='A16S')
+        return context
+
+
+class AmpliconDetailView(DetailView):
     model = AmpliconSequencingMetadata
     context_object_name = 'metadata'
     template_name = 'base_amplicon/metadata_detail.html'
 
     def get_context_data(self, **kwargs):
-        context = super(MetadataView, self).get_context_data(**kwargs)
+        context = super(AmpliconDetailView, self).get_context_data(**kwargs)
         context['sequencefiles'] = AmpliconSequenceFile.objects.filter(sample__bpa_id=context['metadata'].bpa_id)
         context['sample'] = context['metadata']  # same name to make common sequence file template work
         return context
+
+
