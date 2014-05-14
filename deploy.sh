4096bff8a610c425446a20e31c8b309546e8915c
Thys Meintjes <tmeintjes@ccg.murdoch.edu.au>
2014-05-14 13:35:30 +0800

    Updated deploy

--- deploy.sh
+++ deploy.sh
@@ -95,8 +95,8 @@ nuclear() {
     ${CMD} runscript ingest_base_metagenomics --traceback
     ${CMD} runscript ingest_landuse --traceback
     ${CMD} runscript ingest_base_contextual --traceback
-    ${CMD} runscript ingest_base_otu_fakedata --traceback	
     ${CMD} runscript ingest_base_amplicon --traceback
+    ${CMD} runscript ingest_base_otu --traceback
 
     # links
     bpam runscript url_checker
