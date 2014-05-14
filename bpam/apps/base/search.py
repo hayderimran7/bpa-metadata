066e4e97c08d968ce685654e8b81105839214537
Lee Render <lrender@ccg.murdoch.edu.au>
2014-05-14 12:01:08 +0800

    BM-103 Big(!) speed improvement on taxonomic search

--- bpam/apps/base/search.py
+++ bpam/apps/base/search.py
@@ -306,8 +306,10 @@ class Searcher(object):
             otus = OperationalTaxonomicUnit.objects.filter(reduce(and_, [Q(tf) for tf in taxonomy_filters]))
             logger.debug("otus matching taxonomic filters = %s" % otus)
             from apps.base_otu.models import SampleOTU
-            sample_otus = SampleOTU.objects.filter(sample__bpa_id__in=bpa_ids).filter(otu__in=otus).order_by('sample__bpa_id')
-            return set([so.sample.bpa_id for so in sample_otus])
+            #sample_otus = SampleOTU.objects.filter(sample__bpa_id__in=bpa_ids).filter(otu__in=otus).order_by('sample__bpa_id')
+            #return set([so.sample.bpa_id for so in sample_otus])
+            data = SampleOTU.objects.filter(sample__bpa_id__in=bpa_ids).filter(otu__in=otus).values_list('sample__bpa_id', flat=True).distinct()
+            return BPAUniqueID.objects.filter(bpa_id__in=data)
         else:
             logger.debug("no taxonomy filtering will be applied")
             return bpa_ids
