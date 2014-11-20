dc099c692090acdda6ea5bd269becc5f833a442e
Thys Meintjes <tmeintjes@ccg.murdoch.edu.au>
2014-11-20 10:46:22 +0800

    BM-175 Adds contact details for Andrew Bisett

--- bpam/apps/base/views.py
+++ bpam/apps/base/views.py
@@ -331,7 +331,7 @@ class TaxonomyLookUpView(View):
 
 
 class ContactsView(TemplateView):
-    template_name = 'contacts.html'
+    template_name = 'base/contacts.html'
 
 
 class RequestAccess(TemplateView):
