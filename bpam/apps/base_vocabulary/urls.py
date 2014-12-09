25d3f2c6d871d4c7c149bc5baa74942844421082
Thys Meintjes <tmeintjes@ccg.murdoch.edu.au>
2014-12-09 14:49:53 +0800

    Adds all remaining BASE vocabulary types to WEB API

--- bpam/apps/base_vocabulary/urls.py
+++ bpam/apps/base_vocabulary/urls.py
@@ -6,17 +6,52 @@ urlpatterns = patterns(
     "",
     url(
         regex=r'^landuse/$',
-        view=views.LandUseCreateReadView.as_view(),
+        view=views.LandUseReadView.as_view(),
         name='landuse_rest_api'
     ),
     url(
         regex=r'^soiltexture/$',
-        view=views.SoilTextureCreateReadView.as_view(),
+        view=views.SoilTextureReadView.as_view(),
         name='soiltexture_rest_api'
     ),
     url(
         regex=r'^soilcolour/$',
-        view=views.SoilColourCreateReadView.as_view(),
+        view=views.SoilColourReadView.as_view(),
         name='soilcolour_rest_api'
+    ),
+      url(
+        regex=r'^generalecologicalzone/$',
+        view=views.GeneralEcologicalZoneReadView.as_view(),
+        name='generalecologicalzone_rest_api'
+    ),
+    url(
+        regex=r'^tillagetype/$',
+        view=views.TillageTypeReadView.as_view(),
+        name='tillagetype_rest_api'
+    ),
+    url(
+        regex=r'^horizonclassification/$',
+        view=views.HorizonClassificationReadView.as_view(),
+        name='horizonclassification_rest_api'
+    ),
+    url(
+        regex=r'^australiansoilclassification/$',
+        view=views.AustralianSoilClassificationReadView.as_view(),
+        name='australiansoilclassification_rest_api'
+    ),
+    url(
+        regex=r'^faosoilclassification/$',
+        view=views.FAOSoilClassificationReadView.as_view(),
+        name='faosoilclassification_rest_api'
+    ),
+    url(
+        regex=r'^drainageclassification/$',
+        view=views.DrainageClassificationReadView.as_view(),
+        name='drainageclassification_rest_api'
+    ),
+    url(
+        regex=r'^profileposition/$',
+        view=views.ProfilePositionReadView.as_view(),
+        name='profileposition_rest_api'
     )
 )
--- bpam/apps/base_vocabulary/views.py
+++ bpam/apps/base_vocabulary/views.py
@@ -1,22 +1,64 @@
-from rest_framework import viewsets
 from rest_framework import generics
 
 from .models import *
 import serializers
 
-class LandUseCreateReadView(generics.ListCreateAPIView):
+
+class LandUseReadView(generics.ListAPIView):
     queryset = LandUse.objects.all()
     serializer_class = serializers.LandUseSerializer
     paginate_by = 100
 
 
-class SoilTextureCreateReadView(generics.ListCreateAPIView):
+class SoilTextureReadView(generics.ListAPIView):
     queryset = SoilTexture.objects.all()
     serializer_class = serializers.SoilTextureSerializer
     paginate_by = 100
 
 
-class SoilColourCreateReadView(generics.ListCreateAPIView):
+class SoilColourReadView(generics.ListAPIView):
     queryset = SoilColour.objects.all()
     serializer_class = serializers.SoilColourSerializer
+    paginate_by = 100
+
+
+class GeneralEcologicalZoneReadView(generics.ListAPIView):
+    queryset = GeneralEcologicalZone.objects.all()
+    serializer_class = serializers.GeneralEcologicalZoneSerializer
+    paginate_by = 100
+
+
+class TillageTypeReadView(generics.ListAPIView):
+    queryset = TillageType.objects.all()
+    serializer_class = serializers.TillageTypeSerializer
+    paginate_by = 100
+
+
+class HorizonClassificationReadView(generics.ListAPIView):
+    queryset = HorizonClassification.objects.all()
+    serializer_class = serializers.HorizonClassificationSerializer
+    paginate_by = 100
+
+
+class AustralianSoilClassificationReadView(generics.ListAPIView):
+    queryset = AustralianSoilClassification.objects.all()
+    serializer_class = serializers.AustralianSoilClassificationSerializer
+    paginate_by = 100
+
+
+class FAOSoilClassificationReadView(generics.ListAPIView):
+    queryset = FAOSoilClassification.objects.all()
+    serializer_class = serializers.FAOSoilClassificationSerializer
+    paginate_by = 100
+
+
+class DrainageClassificationReadView(generics.ListAPIView):
+    queryset = DrainageClassification.objects.all()
+    serializer_class = serializers.DrainageClassificationSerializer
+    paginate_by = 100
+
+
+class ProfilePositionReadView(generics.ListAPIView):
+    queryset = ProfilePosition.objects.all()
+    serializer_class = serializers.ProfilePositionSerializer
     paginate_by = 100
