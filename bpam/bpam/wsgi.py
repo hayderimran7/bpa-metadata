a71b89eda776eb3654fb6cc6f90fbc0e4d31f630
Adam Hunter <ahunter@ccg.murdoch.edu.au>
2015-01-02 00:34:16 +0800

    wsgi: configure env before invoking application

--- bpam/bpam/wsgi.py
+++ bpam/bpam/wsgi.py
@@ -31,12 +31,9 @@ path.append(SITE_ROOT)
 # setting points here.
 from django.core.wsgi import get_wsgi_application
 
-_application = get_wsgi_application()
 
 from ccg_django_utils.conf import setup_prod_env
-
-setup_prod_env(os.path.basename(os.path.dirname(__file__)))
-
+setup_prod_env("bpam")
 
 def application(wenv, start_response):
     # Before entering the django app, transfer the SCRIPT_NAME http
@@ -45,4 +42,4 @@ def application(wenv, start_response):
     if mount_point:
         os.environ["SCRIPT_NAME"] = mount_point
         wenv["HTTP_SCRIPT_NAME"] = wenv["SCRIPT_NAME"] = mount_point
-    return _application(wenv, start_response)
+    return get_wsgi_application()(wenv, start_response)
