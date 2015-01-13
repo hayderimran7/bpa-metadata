4d72165a168ce7c53d0ac3f23433194475eef086
Thys Meintjes <tmeintjes@ccg.murdoch.edu.au>
2015-01-13 11:42:23 +0800

    Adds debug_toolbar if not already added if asked to.

--- bpam/bpam/settings.py
+++ bpam/bpam/settings.py
@@ -356,15 +356,13 @@ if env.get("DEBUG_TOOLBAR", False):
     INSTALLED_APPS += ('debug_toolbar', )
     MIDDLEWARE_CLASSES += ('debug_toolbar.middleware.DebugToolbarMiddleware', )
     INTERNAL_IPS = ('127.0.0.1', '172.16.2.189',)  # explicitly set this for your environment
-    INSTALLED_APPS += (
-        'debug_toolbar',
-    )
 
 if env.get("BPA_TEST", False):
     DEBUG = True
     TEMPLATE_DEBUG = DEBUG
 
-    INSTALLED_APPS += ('debug_toolbar',)
+    if not INSTALED_APPS.contains('debug_toolbar'):
+        INSTALLED_APPS += ('debug_toolbar',)
     INTERNAL_IPS = ('127.0.0.1',)
 
     MIDDLEWARE_CLASSES += ('debug_toolbar.middleware.DebugToolbarMiddleware', )
