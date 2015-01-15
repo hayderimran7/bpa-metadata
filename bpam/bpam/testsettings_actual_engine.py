dcb50b272de4e533e40090fafe1056ea979b9cf7
Thys Meintjes <tmeintjes@ccg.murdoch.edu.au>
2015-01-15 13:12:40 +0800

    Test against target db engine

---
+++ bpam/bpam/testsettings_actual_engine.py
@@ -0,0 +1,15 @@
+# this settings runs the tests against the real db engine
+
+from testsettings import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': env.get_db_engine("dbtype", "pgsql"),
+        'NAME': 'test-bpam',
+        'USER': 'bpam',
+        'PASSWORD': 'test-bpam',
+        'HOST': '',
+        'PORT': '',
+    }
+}
+
