40c03f5dd9012d5f0ea154d98f5a057ebf2255a1
Thys Meintjes <tmeintjes@ccg.murdoch.edu.au>
2015-10-01 14:26:34 +0800

    GBR small export and data display changes

--- bpam/apps/gbr/data_export.py
+++ bpam/apps/gbr/data_export.py
@@ -6,17 +6,28 @@ from .models import GBRSample
 from .models import GBRSequenceFile
 
 # samples
-
 sample_values = (
         "bpa_id",
         "name",
         "dna_source__description",
         "organism__genus",
         "organism__species",
+        "dataset",
+        "dna_extraction_protocol",
+        "requested_read_length",
+        "sequencing_facility__name",
         "collection_event__site__site_name",
         "collection_event__site__lat",
         "collection_event__site__lon",
         "collection_event__collection_date",
+        "collection_event__water_temp",
+        "collection_event__water_ph",
+        "collection_event__depth",
+        "collection_event__note",
+        "sequencing_notes",
+        "collection_event__collector__first_name",
+        "collection_event__collector__last_name",
+        "collection_event__collector__email",
         )
 
 sample_headers = {
@@ -24,10 +35,18 @@ sample_headers = {
         "dna_source__description": "DNA/RNA Source",
         "organism__genus": "Genus",
         "organism__species": "Species",
+        "sequencing_facility__name": "Facility",
         "collection_event__site__site_name": "Site Name",
         "collection_event__site__lat": "Latitude",
         "collection_event__site__lon": "Longitude",
         "collection_event__collection_date": "Collection Date",
+        "collection_event__water_temp": "Water Temperature",
+        "collection_event__water_ph": "Water pH",
+        "collection_event__depth": "Water Depth (m)",
+        "collection_event__note": "Note",
+        "collection_event__collector__first_name": "Collector Name",
+        "collection_event__collector__last_name": "Collector Last Name",
+        "collection_event__collector__email": "Collector email"
         }
 
 def get_samples(response):
@@ -42,7 +61,7 @@ sequence_file_headers = {
     "sample__collection_event__site__site_name": "Collection Site Name",
     "sample__collection_event__site__lat": "Latitude",
     "sample__collection_event__site__lon": "Longitue",
-    "sample__collection_date": "Collection date",
+    "sample__collection_event__collection_date": "Collection date",
     "sample__name": "Sample Name",
     }
 
@@ -54,7 +73,7 @@ sequence_file_values = (
     "sample__collection_event__site__site_name",
     "sample__collection_event__site__lat",
     "sample__collection_event__site__lon",
-    "sample__collection_date",
+    "sample__collection_event__collection_date",
     "sample__name",
     )
 
