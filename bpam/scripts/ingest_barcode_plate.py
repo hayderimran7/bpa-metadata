fd9203cccbceee23fb9137b5a93593dc3327631a
Thys Meintjes <tmeintjes@ccg.murdoch.edu.au>
2015-10-15 15:52:49 +0800

    Init barcode ingestor

---
+++ bpam/scripts/ingest_barcode_plate.py
@@ -0,0 +1,57 @@
+import csv
+
+from libs import ingest_utils logger_utils
+from libs.fetch_data import Fetcher
+from apps.bpaauth.models import BPAUser
+
+logger = logger_utils.get_logger(__name__)
+
+METADATA_URL = 'https://downloads.bioplatforms.com/bpa/barcode/plate/'
+DATA_DIR = Path(ingest_utils.METADATA_ROOT, 'barcode/plate/')
+
+
+def get_data(users_file):
+    with open(users_file, 'rb') as contacts:
+        fieldnames = ['Project', 'First name', 'Last name', 'Organisation', 'Email', 'Interest', 'Lab']
+        reader = csv.DictReader(contacts, fieldnames=fieldnames, restkey='therest')
+        return ingest_utils.strip_all(reader)
+
+
+def filter_contacts(contact):
+    """
+    If for some reason the contact line is unsuitable, filter it out.
+    """
+    username = user_helper.make_username(contact)
+    if not username:
+        return True
+
+    if contact['Project'].strip() == 'Project':
+        return True
+
+    return False
+
+
+def ingest_contacts(users_file):
+    """
+    Contacts associated with BPA projects
+    """
+
+    for contact in get_data(users_file):
+        if filter_contacts(contact):
+            continue
+
+        # if the user already exists, she probably is part of several projects so add her to those groups
+        username = user_helper.make_username(contact)
+        try:
+            existing_user = BPAUser.objects.get(username=username)
+            group = user_helper.get_group(contact['Project'])
+            existing_user.groups.add(group)
+            existing_user.save()
+        except BPAUser.DoesNotExist:
+            user_helper.make_new_user(username, contact)
+
+
+def run():
+    fetcher = Fetcher(DATA_DIR, METADATA_URL)
+    fetcher.fetch(BPA_USERS_FILE)
+    ingest_contacts(DATA_DIR + BPA_USERS_FILE)
