725c7b69b6505e78dc143e500e90258bbd2692b2
Thys Meintjes <tmeintjes@ccg.murdoch.edu.au>
2016-04-26 11:32:01 +0800

    Adds RSA ID to common BPA ID

---
+++ bpam/apps/common/migrations/0002_bpauniqueid_sra_id.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('common', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='bpauniqueid',
+            name='sra_id',
+            field=models.CharField(null=True, max_length=12, blank=True, help_text=b'SRA ID', unique=True, verbose_name='SRA ID'),
+        ),
+    ]
