faf09b753cbb150848d2e2df13f909349919ea2a
Thys Meintjes <tmeintjes@ccg.murdoch.edu.au>
2016-04-26 12:25:49 +0800

    SRA to BPA-ID map

---
+++ bpam/scripts/ingest_base_sra_id.py
@@ -0,0 +1,43 @@
+import csv
+import sys
+
+from libs import ingest_utils, user_helper, logger_utils
+from libs.fetch_data import Fetcher
+from apps.common.models import BPAUniqueID
+from unipath import Path
+
+logger = logger_utils.get_logger(__name__)
+
+METADATA_URL = 'https://downloads.bioplatforms.com/base/metadata/'  # the folder
+SAMPLE_ID_FILE = 'Biosample_accessions.csv'  # the file
+DATA_DIR = Path(ingest_utils.METADATA_ROOT, 'biosample_id/')
+
+
+def get_data(_file):
+    with open(_file, 'rb') as ids:
+        fieldnames = ['Accession', 'Sample Name']
+        reader = csv.DictReader(ids, fieldnames=fieldnames)
+        return ingest_utils.strip_all(reader)
+
+
+def ingest_ids(ids_file):
+    """ Biosample_accessions ID's assigned by SRA """
+
+    for _map in get_data(ids_file):
+        bpa_id = _map.get('Sample Name')
+        bpa_id = bpa_id.replace('/', '.')
+        sra_id = _map.get('Accession')
+        try:
+            bpa = BPAUniqueID.objects.get(bpa_id=bpa_id)
+            bpa.sra_id = sra_id
+            bpa.save()
+        except BPAUniqueID.DoesNotExist as e:
+            logger.info(bpa_id)
+            logger.info(sra_id)
+            logger.error(e)
+
+
+def run():
+    fetcher = Fetcher(DATA_DIR, METADATA_URL, auth=('base', 'b4s3'))
+    fetcher.fetch(SAMPLE_ID_FILE)
+    ingest_ids(DATA_DIR + SAMPLE_ID_FILE)
