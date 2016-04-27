cc04221210175e9d9915f8fc916cef70a9c07d03
Grahame Bowland <grahame@angrygoats.net>
2016-04-27 15:10:22 +0800

    update Meta on BPAMirror

---
+++ bpam/apps/common/migrations/0005_auto_20160427_1509.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('common', '0004_bpamirror_order'),
+    ]
+
+    operations = [
+        migrations.AlterModelOptions(
+            name='bpamirror',
+            options={'ordering': ['order']},
+        ),
+    ]
