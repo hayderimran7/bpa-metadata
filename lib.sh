a5b9060e188bc14d7e685302e67fa33632c28fa3
Thys Meintjes <tmeintjes@ccg.murdoch.edu.au>
2016-05-06 07:51:34 +0800

    Moves usage message from lib to develop.sh

--- lib.sh
+++ lib.sh
@@ -27,51 +27,6 @@ DOCKER_RUN_OPTS='-e PIP_INDEX_URL -e PIP_TRUSTED_HOST'
 DOCKER_COMPOSE_BUILD_OPTS=''
 
 
-usage() {
-    cat << EOF
-  Wrapper script to call common tools while developing ${PROJECT_NAME}
-
-  Environment:
-  Pull during docker build   DOCKER_PULL                 ${DOCKER_PULL}
-  No cache during build      DOCKER_NO_CACHE             ${DOCKER_NO_CACHE}
-  Use proxy during builds    DOCKER_BUILD_PROXY          ${DOCKER_BUILD_PROXY}
-  Push/pull from docker hub  DOCKER_USE_HUB              ${DOCKER_USE_HUB}
-  Release docker image       DOCKER_IMAGE                ${DOCKER_IMAGE}
-  Use a http proxy           SET_HTTP_PROXY              ${SET_HTTP_PROXY}
-  Use a pip proxy            SET_PIP_PROXY               ${SET_PIP_PROXY}
-  Use mailgun to send mail   DJANGO_MAILGUN_API_KEY      ${DJANGO_MAILGUN_API_KEY}
-  Use mailgun to send mail   DJANGO_MAILGUN_SERVER_NAME  ${DJANGO_MAILGUN_SERVER_NAME}
-
-  Usage: ${PROGNAME} options
-
-  OPTIONS:
-  dev            Pull up stack and start developing
-  dev_build      Build dev stack images
-  prod_build     Build production image from current tag or branch
-  baseimage      Build base image
-  buildimage     Build build image
-  devimage       Build dev image
-  releaseimage   Build release image
-  releasetarball Produce release tarball artifact
-  shell          Create and shell into a new web image, used for db checking with Django env available
-  superuser      Create Django superuser
-  runscript      Run one of the available scripts
-  checksecure    Run security check
-  up             Spins up docker development stack
-  rm             Remove all containers
-  pythonlint     Run python lint
-  unit_tests     Run unit tests
-  usage          Print this usage
-  help           Print this usage
-
-
-  Example, start dev with no proxy and rebuild everything:
-  SET_PIP_PROXY=0 SET_HTTP_PROXY=0 ${PROGNAME} dev_rebuild
-  ${PROGNAME} dev_build
-  ${PROGNAME} dev
-EOF
-    exit 1
-}
 
 # logs and feedback
 info () {
