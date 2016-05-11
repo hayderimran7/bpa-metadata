40f5a1099cfccfa4ab4af32b4e5c2b2f59492be0
Thys Meintjes <tmeintjes@ccg.murdoch.edu.au>
2016-05-11 16:19:40 +0800

    Set vocabs using management command

--- bpam/apps/base_vocabulary/management/commands/set_vocabulary.py
+++ bpam/apps/base_vocabulary/management/commands/set_vocabulary.py
@@ -9,15 +9,27 @@ from apps.base_vocabulary.models import *
 # logging.basicConfig(level=logging.INFO)
 logger = get_logger(__name__)
 
+
+def _drainage():
+    """Set Drainage Classification"""
+    logger.info(_drainage.__doc__)
+    for drainage, description in DrainageClassificationVocabulary:
+        DrainageClassification.objects.get_or_create(drainage=drainage, description=description)
+
+def _soilcolour():
+    """Set Soil Colour Vocabulary """
+    logger.info(_soilcolour.__doc__)
+    for colour, code in SoilColourVocabulary:
+        SoilColour.objects.get_or_create(colour=colour, code=code)        
+
 def _tillage():
     """Set Tillage Classification Vocabulary """
-    
     logger.info(_tillage.__doc__)
     for tillage, description in TillageClassificationVocabulary:
         TillageType.objects.get_or_create(tillage=tillage, description=description)
-        
-        
+
 def _aus_soil():
+    """ Australian Soil Classification """
     logger.info(_aus_soil.__doc__)
     for classification, note in AustralianSoilClassificationVocabulary:
         logger.info(classification)
@@ -26,6 +38,7 @@ def _aus_soil():
         soil.save()
 
 def _fao_soil():
+    """ FAO Soil Classification """
     logger.info(_fao_soil.__doc__)
     for classification, note in FAOSoilClassificationVocabulary:
         logger.info(classification)
@@ -40,11 +53,9 @@ def _profileposition():
     for position in ProfilePositionVocabulary:
         logger.info(position)
         ProfilePosition.objects.get_or_create(position=position)
-        
 
 def _broad_vegetation_type():
     """Set Broadvegetation Type Vocabulary """
-    
     logger.info(_broad_vegetation_type.__doc__)
     for vegetation, note in EcologicalZoneVocabulary:
         logger.info(vegetation)
@@ -55,7 +66,6 @@ def _broad_vegetation_type():
 
 def _ecozone():
     """Set Ecological Zone Vocabulary """
-    
     logger.info(_ecozone.__doc__)
     for description, note in EcologicalZoneVocabulary:
         logger.info(description)
@@ -65,17 +75,13 @@ def _ecozone():
         
 def _horizon():
     """Set Horizon Classification Vocabulary """
-    
     logger.info(_horizon.__doc__)
     for c, description in HorizonClassificationVocabulary:
         logger.info(description)
         HorizonClassification.objects.get_or_create(horizon=c, description=description)
-        
-    
 
 def _landuse():
     """ Land Use vocabulary """
-    
     logger.info(_landuse.__doc__)
     for u1 in LandUseVocabulary:
         parent_name_u1 = u1[0]
@@ -101,3 +107,5 @@ class Command(BaseCommand):
         _aus_soil()
         _fao_soil()
         _tillage()
+        _soilcolour()
+        _drainage()
