2bcd6ae545babb8a59ab2f403aa8eb685243845b
Thys Meintjes <tmeintjes@ccg.murdoch.edu.au>
2016-06-13 00:23:39 +0800

    All counts in

--- bpam/bpam/views.py
+++ bpam/bpam/views.py
@@ -4,6 +4,9 @@ from apps.melanoma.models import MelanomaSample
 from apps.sepsis.models import SepsisSample
 from apps.gbr.models import GBRSample
 from apps.base.models import BASESample
+from apps.wheat_cultivars.models import CultivarSample
+from apps.wheat_pathogens.models import PathogenSample as WheatPathogenSample
+from apps.wheat_pathogens_transcript.models import WheatPathogenTranscriptSample
 
 
 class LandingView(TemplateView):
@@ -15,5 +18,8 @@ class LandingView(TemplateView):
         context['sepsis_sample_count'] = SepsisSample.objects.count()
         context['gbr_sample_count'] = GBRSample.objects.count()
         context['base_sample_count'] = BASESample.objects.count()
+        context['wheat_cultivars_sample_count'] = CultivarSample.objects.count()
+        context['wheat_pathogens_genome_sample_count'] = WheatPathogenSample.objects.count()
+        context['wheat_pathogens_transcript_sample_count'] = WheatPathogenTranscriptSample.objects.count()
 
         return context
