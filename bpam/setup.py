c0283ff7dc08a13bb0e9c3065caa5a33e35e56aa
Thys Meintjes <tmeintjes@ccg.murdoch.edu.au>
2016-06-29 10:24:52 +0800

    Version 4.0.8 Sulphur admin fix

--- bpam/setup.py
+++ bpam/setup.py
@@ -24,7 +24,7 @@ package_scripts = ["manage.py"]
 package_data = get_data_files()
 
 setup(name='bpam',
-      version='4.0.7',
+      version='4.0.8',
       description="BPA Metadata Management",
       author='Centre for Comparative Genomics',
       author_email='web@ccg.murdoch.edu.au',
