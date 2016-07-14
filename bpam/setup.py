17b36c47a63244681ca35e94d2552e9860cb82ea
Thys Meintjes <tmeintjes@ccg.murdoch.edu.au>
2016-07-14 11:22:11 +0800

    Version 4.0.20

--- bpam/setup.py
+++ bpam/setup.py
@@ -24,7 +24,7 @@ package_scripts = ["manage.py"]
 package_data = get_data_files()
 
 setup(name='bpam',
-      version='4.0.19',
+      version='4.0.20',
       description="BPA Metadata Management",
       author='Centre for Comparative Genomics',
       author_email='web@ccg.murdoch.edu.au',
