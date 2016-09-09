bd7ed57db72647f4dd953ae29245cc973069695b
Tamas Szabo <tszabo@ccg.murdoch.edu.au>
2016-09-09 10:37:12 +0300

    Version bump 4.0.31 -> 4.0.32

--- bpam/setup.py
+++ bpam/setup.py
@@ -24,7 +24,7 @@ package_scripts = ["manage.py"]
 package_data = get_data_files()
 
 setup(name='bpam',
-      version='4.0.31',
+      version='4.0.32',
       description="BPA Metadata Management",
       author='Centre for Comparative Genomics',
       author_email='web@ccg.murdoch.edu.au',
