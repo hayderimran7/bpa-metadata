327389954ae0f41fbfadd49483129f89c4e3a2b7
Grahame Bowland <grahame@angrygoats.net>
2017-03-22 11:49:12 +0800

    BASE info page to CKAN

--- bpam/apps/base/urls.py
+++ bpam/apps/base/urls.py
@@ -5,11 +5,9 @@ from bpam.decorators import DEBUG_ONLY_VIEW
 import views
 
 urlpatterns = [
-    url(r'^$', DEBUG_ONLY_VIEW(views.BaseView.as_view()), name='index'),
     url(r'^search/?$', views.BASESearchView.as_view(), name='search'),
     url(r'^lookup/(?P<search_field>.*)', views.StandardisedVocabularyLookUpView.as_view(), name='lookup'),
     url(r'^taxonomy/(?P<level>.*)/(?P<taxon>.*)', views.TaxonomyLookUpView.as_view(), name='taxonomy'),
-    url(r'^information$', views.InfoView.as_view(), name='information'),
     url(r'^requestaccess', views.RequestAccessView.as_view(), name='requestaccess'),
     url(r'^searchexport', views.SearchExportView.as_view(), name='searchexport'),
     url(r'^docs/', include('rest_framework_swagger.urls')),
