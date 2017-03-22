5134a11fb1636a130e0036c1315e259bd026548b
Grahame Bowland <grahame@angrygoats.net>
2017-03-22 13:48:55 +0800

    reinstate the BASE index page

--- bpam/apps/base/urls.py
+++ bpam/apps/base/urls.py
@@ -5,6 +5,7 @@ from bpam.decorators import DEBUG_ONLY_VIEW
 import views
 
 urlpatterns = [
+    url(r'^$', DEBUG_ONLY_VIEW(views.BaseView.as_view()), name='index'),
     url(r'^search/?$', views.BASESearchView.as_view(), name='search'),
     url(r'^lookup/(?P<search_field>.*)', views.StandardisedVocabularyLookUpView.as_view(), name='lookup'),
     url(r'^taxonomy/(?P<level>.*)/(?P<taxon>.*)', views.TaxonomyLookUpView.as_view(), name='taxonomy'),
