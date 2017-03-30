c1a4d4188bead85199dad6cff60e59a2ae3d2d99
Grahame Bowland <grahame@angrygoats.net>
2017-03-30 13:47:14 +0800

    bump version

--- bpam/setup.py
+++ bpam/setup.py
@@ -24,7 +24,7 @@ package_scripts = ["manage.py"]
 package_data = get_data_files()
 
 setup(name='bpam',
-      version='4.9.1',
+      version='4.10.1',
       description="BPA Metadata Management",
       author='Centre for Comparative Genomics',
       author_email='web@ccg.murdoch.edu.au',
