60e447ab5652948e357e56a776f82a3514883c1e
Tamas Szabo <tszabo@ccg.murdoch.edu.au>
2017-03-30 10:30:54 +0800

    Front-end changes for Stemcells Project Overview #147

--- bpam/apps/stemcell/models.py
+++ bpam/apps/stemcell/models.py
@@ -3,6 +3,21 @@ from django.db import models
 from apps.common.models import BPAUniqueID
 
 
+class NotInDataPortalManager(models.Manager):
+    def get_queryset(self):
+        return super(NotInDataPortalManager, self).get_queryset().filter(in_data_portal=False)
+
+
+class SampleProcessingManager(NotInDataPortalManager):
+    def get_queryset(self):
+        return super(SampleProcessingManager, self).get_queryset().filter(data_generated=False)
+
+
+class BPAArchiveIngestManager(NotInDataPortalManager):
+    def get_queryset(self):
+        return super(BPAArchiveIngestManager, self).get_queryset().filter(data_generated=True)
+
+
 class SampleTrack(models.Model):
 
     _DATA_TYPES = (
@@ -22,15 +37,25 @@ class SampleTrack(models.Model):
     work_order = models.CharField('Work Order', max_length=50, blank=True, null=True)
     contextual_data_submission_date = models.DateField('Contextual Data Submission Date', blank=True, null=True, help_text='YYYY-MM-DD')
     sample_submission_date = models.DateField('Sample Submission Date', blank=True, null=True, help_text='YYYY-MM-DD')
+    data_generated = models.NullBooleanField('Data Generated', default=False)
     dataset_url = models.URLField('Download URL', blank=True, null=True)
     in_data_portal = models.BooleanField('Data ingested into data portal')
     last_modified = models.DateTimeField(auto_now=True)
+
+    @classmethod
+    def get_data_type(cls, data_type_id):
+        return dict(cls._DATA_TYPES).get(data_type_id)
 
     def __unicode__(self):
         return u'{} {}'.format(self.bpa_id, self.omics)
 
     class Meta:
         abstract = True
+
+    objects = models.Manager()
+    uningested = NotInDataPortalManager()
+    sample_processing = SampleProcessingManager()
+    bpa_archive_ingest = BPAArchiveIngestManager()
 
 
 class MetabolomicTrack(SampleTrack):
@@ -71,3 +96,12 @@ class TranscriptomeTrack(SampleTrack):
     class Meta:
         verbose_name = 'Track Transcriptome'
         verbose_name_plural = verbose_name
+
+
+CKAN_RESOURCE_TYPE_TO_MODEL = {
+    'stemcells-metabolomic': MetabolomicTrack,
+    'stemcells-proteomic': ProteomicTrack,
+    'stemcells-transcriptomics': TranscriptomeTrack,
+    'stemcells-smallrna': SmallRNATrack,
+    'stemcells-singlecellrnaseq': SingleCellRNASeqTrack,
+}
