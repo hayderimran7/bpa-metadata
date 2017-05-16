430497a1d931e98327c1e7f74d3c35b1bb0c9ab4
Grahame Bowland <grahame@angrygoats.net>
2017-05-16 10:49:26 +0800

    bpam 4.16.1

--- bpam/setup.py
+++ bpam/setup.py
@@ -24,7 +24,7 @@ package_scripts = ["manage.py"]
 package_data = get_data_files()
 
 setup(name='bpam',
-      version='4.16.0',
+      version='4.16.1',
       description="BPA Metadata Management",
       author='Centre for Comparative Genomics',
       author_email='web@ccg.murdoch.edu.au',
